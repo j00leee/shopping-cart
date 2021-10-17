9df3b0c03bf40e7ec47e73ee603407671932a971
julie chung <julie.minjung.chung@gmail.com>
2021-10-17 18:18:38 -0400

    added logic in controller files

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function cartList()
+    {
+        $cartItems = \Cart::getContent();
+        return view('cart', compact('cartItems'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        \Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->image,
+            )
+            ]);
+            session()->flash('success', 'Product is added to cart successfull!');
+
+            return redirect()->route('cart.list');
+    }
+
+    public function updateCart(Request $request)
+    {
+        \Cart::update(
+            $request->id,
+            [
+                'quantity' => [
+                    'relative' => false,
+                    'value' => $request->quanity
+                ],
+            ]
+        );
+        session()->flash('success', 'Item cart is updated successfully!');
+        
+        return redirect()->route('cart.list');
+    }
+
+    public function removeCart(Request $request)
+    {
+        \Cart::remove($request->id);
+        session()->flash('success', 'Item cart remove successfully!');
+
+        return redirect()->route('cart.list');
+    }
+
+    public function clearAllCart()
+    {
+        \Cart::clear();
+
+        session()->flash('success', 'All Item Cart Clear Successfully!');
+
+        return redirect()->route('cart.list');
+    }
 }
