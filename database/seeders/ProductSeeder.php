f4617247e6a874b19b29edd21622ff462b79be2d
julie chung <julie.minjung.chung@gmail.com>
2021-10-20 13:17:05 -0400

    added more products, made description to show, changed style

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -32,10 +32,40 @@ class ProductSeeder extends Seeder
             'image' => 'https://www.bathandbodyworks.com/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw761f2125/hires/026139371.jpg?sh=1500&sfrm=jpg'
         ]);
         Product::firstOrCreate([
-            'name' => 'You are the one extratest',
-            'price' => 15,
-            'description' => 'aloe + vitamin e shower gel',
-            'image' => 'https://www.bathandbodyworks.com/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw761f2125/hires/026139371.jpg?sh=1500&sfrm=jpg'
+            'name' => 'Eyeliner',
+            'price' => 5,
+            'description' => 'liquid eyeliner',
+            'image' => 'https://cdn.shopify.com/s/files/1/0054/5796/9240/products/MTM-punk-open_3000x3000-903BU-l389Ps-92kWK_3000x.jpg?v=1630365250'
+        ]);
+        Product::firstOrCreate([
+            'name' => 'Dove Shampoo',
+            'price' => 8.14,
+            'description' => 'Daily moisture shampoo',
+            'image' => 'https://m.media-amazon.com/images/I/61Y3WWoRphS._SL1500_.jpg'
+        ]);
+        Product::firstOrCreate([
+            'name' => 'Concealer',
+            'price' => 4,
+            'description' => 'Face concealer',
+            'image' => 'https://www.maybelline.com/~/media/mny/global/face-makeup/concealer/fit-me-concealer/maybelline-fit-me-concealer-15-light-041554247718-c.jpg'
+        ]);
+        Product::firstOrCreate([
+            'name' => 'Face Gel Cream',
+            'price' => 4,
+            'description' => 'Neutrogena',
+            'image' => 'https://ntg-catalog.imgix.net/products/brightboost/6811121-SF10-2.jpg?w=1200&h=1443&sfrm=jpg&fit=crop'
+        ]);
+        Product::firstOrCreate([
+            'name' => 'Sun Block Cream',
+            'price' => 4,
+            'description' => 'Lotus herbals safe sun block cream spf 30',
+            'image' => 'https://m.media-amazon.com/images/I/6141wynCCoL._SL1200_.jpg'
+        ]);
+        Product::firstOrCreate([
+            'name' => 'Soak Infusion',
+            'price' => 25,
+            'description' => 'Infused Bath Salts',
+            'image' => 'https://assets.verishop.com/salt-by-hendrix-soak-infusion-bath-salts/E10036009000000-3180472387.jpg?auto=format&cs=strip&fit=max&w=1200'
         ]);
     }
 }
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container px-6 mx-auto">
-        <h3 class="text-2xl font-medium text-gray-700">Product List</h3>
+        <h3 class="text-2xl font-medium text-gray-700">Cosmetics Product List</h3>
         <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             @foreach ($products as $product)
             <div class="w-full max-w-sm mx-auto overflow-hidden rounded-md shadow-md">
@@ -13,6 +13,7 @@
                 <div class="px-5 py-3">
                     <h3 class="text-gray-700 uppercase">{{ $product->name }}</h3>
                     <span class="mt-2 text-gray-500">${{ $product->price }}</span>
+                    <p class="mt-2 text-gray-500">{{ $product->description }}</p><br/>
                     <form action="{{ route('cart.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <input type="hidden" value="{{ $product->id }}" name="id">
